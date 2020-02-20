81be1c4c6c6b18efc9e016b60ba64ce5471475e9
Oce_Galaxy <magciantype0@gmail.com>
2020-02-20 12:13:05 +1100

    Add files via upload

---
+++ Scenchange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Scenchange : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public void Changescene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+    // Update is called once per frame
+    public void ExitToDekstop()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
