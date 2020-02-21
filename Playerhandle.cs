b5e92a3d7cc12bcee344fc6094019699f50b1177
Oce_Galaxy <magciantype0@gmail.com>
2020-02-21 16:31:13 +1100

    Add files via upload

---
+++ Playerhandle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Playerhandle : Character
+{
+    #region Variables
+    [Header("Physics")]
+    public CharacterController controller;
+    public float gravity = 20f;
+    public Vector3 moveDirection;
+    [Header("levelData")]
+    public int level = 0;
+    public float currentExp, neededExp, maxExp;
+    #endregion
+
+    #region behaviour
+    void Start()
+    {
+        controller = this.gameObject.GetComponent<CharacterController>();
+    }
+    public override void Movement()
+    {
+        base.Movement();
+        if (controller.isGrounded)
+        {
+            moveDirection = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")));
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpspeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+    #endregion
+}
